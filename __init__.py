2675407e7826e0e2bb8736b39baee77017311c4d
BD-Review <84032165+BD-Review@users.noreply.github.com>
2022-01-03 13:00:27 +0100

    Rename

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 bl_info = {
-    "name": "Test [NAME]",
+    "name": "Test Manual Download",
     "author": "BD",
     "description": "",
     "blender": (2, 80, 0),
